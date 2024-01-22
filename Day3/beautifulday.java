d3dbafa0340185b5966013e6ba269bba6fc92277
Aditya Kadam <88039284+AdityaKadam32@users.noreply.github.com>
2024-01-22 22:41:33 +0530

    beautifulday.java

---
+++ Day3/beautifulday.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulDays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER i
+     *  2. INTEGER j
+     *  3. INTEGER k
+     */
+
+    public static int beautifulDays(int i, int j, int k) {
+    // Write your code here
+    int beautifulDays=0;
+    while(i<=j){                               //23+" ="23"
+        String day=i+"";
+        StringBuilder str=new StringBuilder();
+        str.append(i+"");                     //23+""="23"
+         String s =str.reverse().toString();
+        int ans=Math.abs(Integer.parseInt(day)-Integer.parseInt(s));
+        if(ans%k==0) beautifulDays++;
+        i++;
+    }
+    return beautifulDays;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int i = Integer.parseInt(firstMultipleInput[0]);
+
+        int j = Integer.parseInt(firstMultipleInput[1]);
+
+        int k = Integer.parseInt(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
