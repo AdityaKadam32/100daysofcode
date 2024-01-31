296f995068a2a64eeae761c5322ff2013c589d86
Aditya Kadam <88039284+AdityaKadam32@users.noreply.github.com>
2024-01-31 23:43:17 +0530

    house_robber.java

--- day12/house_robber.java
+++ day12/house_robber.java
@@ -1,24 +1,21 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        // Arrays to store all left multiplication
-        int[] left = new int[nums.length];
-
-        //Arrays to store all right multiplication
-        int[] right = new int[nums.length];
-
-        left [0] =1;
-        for(int i = 1; i< nums.length; i++){
-            left[i] = left[i-1] * nums[i-1];
-        }
-        right[nums.length-1]=1;
-        for(int i=nums.length-2; i>-1; i--){
-            right[i] = right[i+1] * nums[i+1];
-        }
-        int [] ans = new int[nums.length];
-        for(int i=0; i< nums.length; i++) {
-            ans[i] = left[i] * right[i];
-        }
-        return ans;
+    public int rob(int[] nums) {
+        // If only 1 element, just return it
+        if (nums.length < 2)
+         return nums[0];
         
+        // create array to store the maximum loot at each index.
+        int[] dp = new int[nums.length];
+
+        //memoize maximum loots at first 2 indexes
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0] , nums[1]);
+
+        // use them to fill complete array
+        for (int i =2; i<nums.length;i++){
+            //core logic
+            dp[i]= Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[nums.length-1];
     }
 }
