0a2345587324d9d7bd88bbaa5f61c224aff220b1
Aditya Kadam <88039284+AdityaKadam32@users.noreply.github.com>
2024-02-06 00:02:42 +0530

    Update wiggle_sort_II.java

--- day17/wiggle_sort_II.java
+++ day17/wiggle_sort_II.java
@@ -21,3 +21,36 @@ class Solution {
         }    
     }
 }
+
+// wiggle sort with order(n) time complexity
+class Solution {
+    public void wiggleSort(int[] nums) {
+        //0 <= nums[i] <= 5000
+        //min = 0, max = 5000
+        //fill max available value on even positions then on odd positions
+        
+        int[] bucket = new int[5001];
+        
+        for(int i: nums)
+            bucket[i]++;
+        
+        int max = 5000;
+        boolean firstPass = true;
+        for(int i = 1 ; i < nums.length; i+=2){
+            
+            
+            while(bucket[max] == 0)
+                max--;
+            
+            nums[i] = max;
+            bucket[max]--;
+            
+            
+            if(firstPass && i+2 >= nums.length){
+                i = -2; // want to set i = 0 but since at the end it does i+2 so making it i = -2 to get resultant = 0
+                firstPass = false;
+            }
+        }
+        
+    }
+}
