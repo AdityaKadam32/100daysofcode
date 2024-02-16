46286dd0b20c365be5e418957c510a0cfcc421ba
Aditya Kadam <88039284+AdityaKadam32@users.noreply.github.com>
2024-02-17 00:20:26 +0530

    Create permutation_II.java

---
+++ Day27/permutation_II.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        Arrays.sort(nums);
+
+        backtrack(resultList, new ArrayList<>(), nums, new boolean[nums.length]);
+        return resultList;
+        
+    }
+    private void backtrack(List<List<Integer>> resultList,
+     ArrayList<Integer> tempList, int[] nums, boolean[] used) {
+        // If we match the length, it is a permutation
+        if (tempList.size() == nums.length && !resultList.contains(tempList)){
+            resultList.add(new ArrayList<>(tempList));
+            return;
+
+        }
+        for(int i=0; i < nums.length; i++) {
+        // skip if we get same element
+        if (used[i]) continue;
+
+        // Add the new element and mark it as used
+        used[i] = true;
+        tempList.add(nums[i]);
+
+        // Go back to try other element
+        backtrack(resultList, tempList, nums,used);
+
+  
+    //remove the element and mark it as unused 
+    used[i] = false;
+    tempList.remove(tempList.size()-1);
+        }
+
+
+    }
+}
