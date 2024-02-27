efffdc6689f31770bd9ca12a9cbc8c648471395c
Aditya Kadam <88039284+AdityaKadam32@users.noreply.github.com>
2024-02-27 23:19:01 +0530

    Create diameter_of_binary_tree.java

---
+++ Day 37/diameter_of_binary_tree.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+    Map<TreeNode, Integer> map = new HashMap<>();
+    Stack<TreeNode> stack = new Stack<>();
+    int diameter = 0;
+
+    if (root != null)
+      stack.push(root);
+
+    while (!stack.isEmpty()) {
+      TreeNode node = stack.peek();
+
+      // Fill up stack to perform post-order traversal
+      if (node.left != null && !map.containsKey(node.left)) {
+        stack.push(node.left);
+      } else if (node.right != null && !map.containsKey(node.right)) {
+        stack.push(node.right);
+      } else {
+
+        // Process the root, once left and right sub-tree have been processed
+        stack.pop();
+        int leftDepth = map.getOrDefault(node.left, 0);
+        int rightDepth = map.getOrDefault(node.right, 0);
+
+        // Put the max depth at a node in the map
+        map.put(node, 1 + Math.max(leftDepth, rightDepth));
+
+        // Update the max diameter found so far
+        diameter = Math.max(diameter, leftDepth + rightDepth);
+      }
+    }
+    return diameter;
+  }
+
+}
