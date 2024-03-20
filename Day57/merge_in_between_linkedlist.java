310ffa9ab81a16cb97c5e5be46cadd685f002941
Aditya Kadam <88039284+AdityaKadam32@users.noreply.github.com>
2024-03-20 23:58:31 +0530

    Create merge_in_between_linkedlist.java

---
+++ Day57/merge_in_between_linkedlist.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode prevNodeA = list1;
+        ListNode nodeB = list1;
+
+        for(int index = 0; index< b; index++){
+            if(index == a-1){
+                prevNodeA = nodeB;
+            }
+            nodeB = nodeB.next;
+        }
+         ListNode list2End = list2;
+         while(list2End.next != null){
+            list2End = list2End.next;
+         }
+
+         prevNodeA.next = list2;
+         list2End.next = nodeB.next;
+         nodeB.next = null;
+         return list1;
+        
+        } 
+        
+}
